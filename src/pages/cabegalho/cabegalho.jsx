d862539df654d7e949c64de66cff11d3d1b4e1e5
gusttavo-ferreira1409 <gustavoocferreira90@gmail.com>
2024-10-23 17:44:13 -0300

    contato

--- src/pages/cabegalho/cabegalho.jsx
+++ src/pages/cabegalho/cabegalho.jsx
@@ -1,42 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './cabegalho.scss';
 import logo from '../image/Sabores da rua.png';
 
+
+
 const Cabecalho = () => {
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
-
   return (
     <div className="header">
       <img src={logo} alt="Logo da Confeitaria" className="logo" />
-      <div className="menu">
-        <div className="dropdown" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
-          <button className="dropdown-button">Produtos</button>
-          {isDropdownOpen && (
-            <div className="dropdown-content">
-              <Link to="/Produtos/Categorias" className="produto-link">Categorias</Link>
-              <Link to="/Produtos/Novidades" className="produto-link">Novidades</Link>
-              <Link to="/Produtos/Promoções" className="produto-link">Promoções</Link>
-              <Link to="/Produtos/Favoritos" className="produto-link">Favoritos</Link>
-            </div>
-          )}
+      <div className="nav">
+        <div>
+          <Link to="/Sobre" className="sobre">Sobre nós</Link>
         </div>
         <div>
-          <Link to="/Contato" className="contato">Contato</Link>
+          <Link to="/Produtos" className="produtos">Produtos</Link>
         </div>
         <div>
           <Link to="/Carrinho" className="carrinho">Carrinho</Link>
         </div>
         <div>
-          <Link to="/sobre" className="sobre">Sobre nós</Link>
-        </div>
-        <div>
           <Link to="/Login" className="login">Login</Link>
         </div>
+        <div className="contato">
+          <Link to="/Contato">Contato</Link>
+        </div>
+
       </div>
     </div>
   );
--- src/pages/contato/contatos.jsx
+++ src/pages/contato/contatos.jsx
@@ -1,65 +1,46 @@
 import React from 'react';
-import './contato.scss';
-import Cabegalho from '../cabegalho/cabegalho'
+import './contatos.scss';
+import Cabecalho from '../cabegalho/cabegalho';
+import logo from '../image/Sabores da rua.png';
+import x from '../image/x.png';
+import wats from '../image/wats.png';
+import face from '../image/face.png';
 
 const Contato = () => {
-   
-   
   return (
-    <><Cabegalho/>
-    <div className="contact-container">
-      <div className=''header> 
- 
+    <>
+      <Cabecalho />
+      <div className='contato-home'>
+    
+       <h1 className='titulo'>contatos</h1>
+        
+        <div className='logo1'>
+          <img src={logo} alt="Logo da Confeitaria" />
+        </div>
+
+        <div className='contato-links'>
+          <div className='face'>
+            <a href="https://facebook.com/confeitaria" target="_blank" rel="noopener noreferrer" aria-label="Facebook da Confeitaria">
+              <img src={face} alt="Facebook" />
+              Facebook
+            </a>
+          </div>
+
+          <div className='x'>
+            <a href="https://twitter.com/confeitaria" target="_blank" rel="noopener noreferrer" aria-label="Twitter da Confeitaria">
+              <img src={x} alt="Twitter" />
+              Twitter
+            </a>
+          </div>
+
+          <div className='wats'>
+          <img className='whats' src={wats} alt="WhatsApp" />
+            <p>WhatsApp</p>
+       
+          </div>
+        </div>
       </div>
-      <h1>Entre em Contato Conosco</h1>
-      <p>Estamos aqui para responder suas dúvidas e atender seus pedidos!</p>
-      juh
-      <div className="contact-info">
-        <p><strong>Telefone:</strong> (xx) xxxx-xxxx</p>
-        <p><strong>E-mail:</strong> contato@confeitaria.com</p>
-      </div>
-      
-      <div className="social-media">
-        <p><strong>Siga-nos:</strong></p>
-        <a href="https://instagram.com/confeitaria" target="_blank" rel="noopener noreferrer">
-          Instagram
-        </a>
-        <a href="https://twitter.com/confeitaria" target="_blank" rel="noopener noreferrer">
-          Twitter
-        </a>
-      </div>
-      
-      <div className="location">
-        <h2>Nossa Localização</h2>
-        <iframe
-          src="link_do_mapa_google"
-          width="100%"
-          height="300"
-          frameBorder="0"
-          allowFullScreen
-          title="Localização"
-        />
-        <p>Endereço: Rua da Confeitaria, 123, Cidade - Estado</p>
-      </div>
-      
-      <div className="hours">
-        <h2>Horário de Funcionamento</h2>
-        <p>Segunda a Sexta: 9h às 18h</p>
-        <p>Sábado: 10h às 16h</p>
-        <p>Domingo: Fechado</p>
-      </div>
-      
-      <div className="contact-form">
-        <h2>Envie uma Mensagem</h2>
-        <form>
-          <input type="text" placeholder="Seu Nome" required />
-          <input type="email" placeholder="Seu E-mail" required />
-          <textarea placeholder="Sua Mensagem" required />
-          <button type="submit">Enviar</button>
-        </form>
-      </div>
-    </div>
-</>    
+    </>
   );
 };
 
